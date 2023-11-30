4b127db6fd4734064d7757c18ec4817414f872a0
Jakub Polacek <Kuba.krtek@email.cz>
2023-11-30 13:46:42 +0100

    Enemy and playerui added

---
+++ js/Game/playerui.js
@@ -0,0 +1,23 @@
+import GameObject from "../Engine/gameobject";
+import UI from "../Engine/ui";
+import Player from "./player";
+
+class PlayerUI extends GameObject
+{
+    constructor(x, y)
+    {
+        super(x, y);
+        this.uiComponent = new UI ("Lives 3 Score 0", x, y);
+        this.addComponent(this.uiComponent);
+    }
+
+    update(deltaTime)
+    {
+        const player = this.game.gameObjects.find((obj) => obj instanceof Player)
+        {
+            this.uiComponent.setText(`Lives: ${player.lives} Score: ${player.score}`);
+            super.update(deltaTime);
+        }
+    }
+}
+export default PlayerUI;
